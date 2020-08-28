310bfc8c36d844799494cb5a8b3bd605a763a549
Vincent Huynh <vin.huynh1@gmail.com>
2020-08-27 19:43:35 -0500

    Updates

---
+++ precache-manifest.782bbea48a4277af36a513a65bb228df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe7f68873a8a9fc14befef2b69a1bf3",
+    "url": "/lofigen-magenta/index.html"
+  },
+  {
+    "revision": "6dee87667ade66a45400",
+    "url": "/lofigen-magenta/static/css/main.a1388623.chunk.css"
+  },
+  {
+    "revision": "9f497795653bd25a6b12",
+    "url": "/lofigen-magenta/static/js/2.d3292597.chunk.js"
+  },
+  {
+    "revision": "e81379d98c3496d82b8299726297c559",
+    "url": "/lofigen-magenta/static/js/2.d3292597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dee87667ade66a45400",
+    "url": "/lofigen-magenta/static/js/main.e46e2015.chunk.js"
+  },
+  {
+    "revision": "3be6cf04b8e9e4f2b29d",
+    "url": "/lofigen-magenta/static/js/runtime-main.02b670aa.js"
+  },
+  {
+    "revision": "6f9ed360d2f8acb324bd8eff83c4ce92",
+    "url": "/lofigen-magenta/static/media/azukiB.6f9ed360.ttf"
+  }
+]);
